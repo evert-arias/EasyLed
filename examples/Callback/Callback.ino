67ff8f472326990d50ca5d4707aac2951b7a8d6b
Evert Arias <evert.arias@hotmail.com>
2019-01-27 00:54:14 -0500

    default led pin changed and comments formated

--- examples/Callback/Callback.ino
+++ examples/Callback/Callback.ino
@@ -4,7 +4,7 @@
   Simple sketch to demostrate how to blink an LED
   and call a function when finished.
 
-  The circuit: An LED attached to pin 2
+  The circuit: An LED attached to pin 27
 
   https://github.com/evert-arias/EasyLed
 
@@ -12,17 +12,17 @@
 
 #include "EasyLed.h"
 
-const int ledPin = 2; // Arduino pin where the LED is connected.
+const int ledPin = 27; // Arduino pin where the LED is connected
 
 EasyLed led(ledPin);
 
 /* function to call when finished */
 void finished() {
   // Blink
-  led.blink(500  /* time on */,
+  led.blink(30  /* time on */,
             100  /* time off */,
-            3    /* cycles */,
-            1000 /* pause between sequences */,
+            2    /* cycles */,
+            500 /* pause between sequences */,
             2    /* sequences */,
             NULL /* function to call when finished */
            );
@@ -35,13 +35,13 @@ void setup() {
             100      /* time off */,
             3        /* cycles */,
             1000     /* pause between sequences */,
-            2        /* sequences */,
+            5        /* sequences */,
             finished /* function to call when finished */
            );
 }
 
 /* loop */
 void loop() {
-  // Always call the "update" function in the loop.
+  // Always call the "update" function in the loop
   led.update();
 }
